213d4005466320a2288b99d6fc8f4774fb5d2593
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-03-31 20:09:27 -0500

    Updates mainjs and prettify all js

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,14 @@
-'use strict';
-//esconder el menu
-//llama el menú
 const accionMenu = document.querySelector("#cabezote");
-//marca la posición previa
-let posicionYprevia = window.pageYOffset;
-//toma el elemento de window y cuando hay cambio ejecuta función
-window.onscroll = function() {
-	//crea la posiscion actual
-	let posicionYactual = window.pageYOffset;
-	// a partir de este punto enpieza a evaluar
-	if (posicionYactual > 100) {
-		// esconde cuando la posicion actual es mayor que la anterior
-		// muestra cuando la posicion anterior es mayor que la actual
-		if (posicionYprevia > posicionYactual) {
-			accionMenu.classList.remove('cabezote--esconder');
-		} else {
-			accionMenu.classList.add('cabezote--esconder');
-		}
-		// reseteea las posiciones
-		posicionYprevia = posicionYactual;
-	};
+let posicionYprevia = window.scrollY;
+window.onscroll = function () {
+  let posicionYactual = window.scrollY;
+  if (posicionYactual > 100) {
+    if (posicionYprevia > posicionYactual) {
+      accionMenu.classList.remove("cabezote--esconder");
+    } else {
+      accionMenu.classList.add("cabezote--esconder");
+    }
+    posicionYprevia = posicionYactual;
+  }
 };
+
